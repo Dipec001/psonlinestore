85b508f21f33597ed132cbb8158dcc22b893480d
Dipec001 <dpecchukwu@gmail.com>
2024-06-17 21:26:39 +0100

    "Added docstrings to views in inventory app to provide API endpoint documentation"

--- inventory/views.py
+++ inventory/views.py
@@ -7,16 +7,32 @@ from .serializers import InventoryItemSerializer, SupplierSerializer
 # Create your views here.
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing Inventory Items.
+
+    This viewset provides CRUD (Create, Retrieve, Update, Delete) operations
+    for Inventory Items.
+    """
     queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        Performs a partial update of an Inventory Item.
+
+        This method allows updating specific fields of an Inventory Item
+        without requiring the entire object data. It checks for empty request data
+        and returns a 400 Bad Request response if no data is provided.
+        """
         if not request.data:
             return Response({"detail": "No data provided for update."}, status=status.HTTP_400_BAD_REQUEST)
         
         return super().partial_update(request, *args, **kwargs)
     
     def destroy(self, request, *args, **kwargs):
+        """
+        Deletes an Inventory Item.
+        """
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({"detail": "Inventory item has been deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
@@ -25,16 +41,34 @@ class InventoryItemViewSet(viewsets.ModelViewSet):
         instance.delete()
 
 class SupplierViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing Suppliers.
+
+    This viewset provides CRUD (Create, Retrieve, Update, Delete) operations
+    for Suppliers.
+    """
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
 
     def partial_update(self, request, *args, **kwargs):
+        """
+        
+        Performs a partial update of a Supplier.
+
+        This method allows updating specific fields of a Supplier
+        without requiring the entire object data. It checks for empty request data
+        and returns a 400 Bad Request response if no data is provided.
+        """
         if not request.data:
             return Response({"detail": "No data provided for update."}, status=status.HTTP_400_BAD_REQUEST)
         
         return super().partial_update(request, *args, **kwargs)
     
     def destroy(self, request, *args, **kwargs):
+        """
+
+        Deletes a Supplier and returns a message to inform of success.
+        """
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({"detail": "Supplier has been deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
@@ -44,6 +78,12 @@ class SupplierViewSet(viewsets.ModelViewSet):
 
 
 class ItemSuppliersView(generics.ListAPIView):
+    """
+    API endpoint to retrieve Suppliers for a specific Inventory Item.
+
+    This view retrieves a list of Suppliers associated with a particular
+    Inventory Item identified by its ID in the URL.
+    """
     serializer_class = SupplierSerializer
 
     def get_queryset(self):
@@ -51,6 +91,9 @@ class ItemSuppliersView(generics.ListAPIView):
         return Supplier.objects.filter(items__id=item_id)
 
 class SupplierItemsView(generics.ListAPIView):
+    """
+    APi endpoint to get all inventory items that belong to a particular supplier identified by it's ID in the URL
+    """
     serializer_class = InventoryItemSerializer
 
     def get_queryset(self):
